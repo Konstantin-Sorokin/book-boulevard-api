4ae25431e1fc946c6fc8ef7cf2ead842a4851521
konstantin <konstantin.69.sorokin@gmail.com>
2024-08-29 18:16:14 +0300

    added router

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
 
+from config import settings
+from api import router
+
 app = FastAPI()
+app.include_router(router)
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app",
+        app=settings.run.app,
         reload=True,
+        host=settings.run.host,
+        port=settings.run.port,
     )
