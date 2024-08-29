5edb92e8cbafca3d8b8ee85c316b704684f438a9
konstantin <konstantin.69.sorokin@gmail.com>
2024-08-29 18:18:59 +0300

    config update

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
 from pydantic_settings import (
     SettingsConfigDict,
     BaseSettings,
@@ -11,9 +11,24 @@ class RunAppConfig(BaseModel):
     app: str = "main:app"
 
 
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
+
 class ApiV1Prefix(BaseModel):
     prefix: str = "/v1"
-    users: str = "/users"
 
 
 class ApiPrefix(BaseModel):
